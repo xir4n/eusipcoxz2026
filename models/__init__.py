117bd2783663ad14cb0bf64f19165d64f43f51bd
Xiran <xiranfr@gmail.com>
2026-05-20 18:25:23 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,2 @@
-from .cp_resnet_bn import Network
 from .learnable_scattering import LearnableScattering
-# from .decimated_learnable_scattering import LearnableScattering_D
-# from .learnable_scattering_mix import LearnableScatteringMix, PsiModDn
-from .cnn import CNN
 from .scattering_transform import ScatteringTransform
-from .gabor import Gabor
-
-ResNet = Network
